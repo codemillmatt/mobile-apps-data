2fde7655fdc7d12b6b42975163093574eb62024f
Matt Soucoup <masoucou@microsoft.com>
2020-06-09 11:53:14 -0700

    finishing demo 4

--- src/demo-4/FitApp/FitApp/Constants.cs
+++ src/demo-4/FitApp/FitApp/Constants.cs
@@ -7,5 +7,8 @@ namespace FitApp.Core
         public static readonly string UserIdPreference = "userid";
 
         public static readonly string DataSyncPointPreference = "syncpoint";
+
+#error Enter your website name
+        public static readonly string WebServerBaseUrl = "ENTER YOUR WEBSITE HERE";
     }
 }
